8698cf2a814c3d224bea90171e7143fcb53fbad3
acanois <david.richter42@gmail.com>
2020-01-24 12:28:39 -0600

    wav to image conversion works properly now

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -7,16 +7,12 @@ from audio_tools import spectrogram_builder as sb
 def build_image_library(folder):
     data_path = os.path.join('data', 'audio')
     curr_path = os.path.join(data_path, folder)
-    file_count = 0
     
     for file in os.listdir(curr_path):
         if file!= '.DS_Store':
             output_path = os.path.join(data_path, folder, file)
-            logger.debug(f'{file_count} / {len(os.listdir(curr_path))}')
             logger.debug(f'processing {output_path}')
-            spectrogram = sb.create_spectrogram(output_path, folder)
-            sb.save_image(spectrogram, folder, file.replace('.wav', '.png'))
-            file_count += 1
+            sb.create_spectrogram(output_path, folder, file.replace('.wav', '.png'))
 
 
 def init_logger():
@@ -34,7 +30,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--fname', help='directory to scan', type=str, required=True)
     args = parser.parse_args()
-    
     logger = init_logger()
     build_image_library(args.fname)
     
--- audio_tools/spectrogram_builder.py
+++ audio_tools/spectrogram_builder.py
@@ -1,12 +1,25 @@
 import os
 import numpy as np
 import librosa
+import librosa.display
 import skimage.io
+import matplotlib.pyplot as plt
 
-def create_spectrogram(filepath, folder, out_name):
+def save_image(spectrogram, folder, out_name):
+    output_path = os.path.join('data', 'images', folder)
+    output_image = librosa.display.specshow(\
+        librosa.power_to_db(spectrogram, ref=np.max),\
+        sr=22050, hop_length=512,\
+        bins_per_octave=12
+    )
+    plt.margins(0)
+    plt.savefig(os.path.join(output_path, out_name), bbox_inches='tight', pad_inches=0.0)
+    plt.close()
+
+def create_spectrogram(filepath, folder):
     """ TODO: DOCS
     """
-    file, samplerate = librosa.load(filepath, sr=None)
-    spectrogram = librosa.feature.melspectrogram(y=file, sr=samplerate)
-    output_path = os.path.join('data', 'images', folder, out_name)
-    skimage.io.imsave(os.path.join(output_path, out_name), spectrogram)
+    file, samplerate = librosa.load(filepath, sr=22050)
+    spectrogram = librosa.feature.melspectrogram(y=file, sr=22050, window='hann')
+
+    return spectrogram
